6249d5f39a58df37e55057893c8c998d78c28ebd
ramaika108 <ramaika108@gmail.com>
2022-07-21 16:13:20 +0530

    Start writing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe ('See if all components of the App rendered', () => {
+    it('Main rendered', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/К СПИСКУ ЮРИДИЧЕСКИХ ЛИЦ/i);
+    expect(headerElement).toBeInTheDocument();
+    })
+
+    it('Aside rendered', () => {
+    render(<App />);
+    const logo1Element = screen.getByText(/ЧЕСТНЫЙ АГЕНТ/);
+    expect(logo1Element).toBeInTheDocument();
+    const logo2Element = screen.getByText(/МЕНЕДЖЕР ПРОЦЕССА/);
+    expect(logo2Element).toBeInTheDocument();
+    })
+
+    it('Footer rendered', () => {
+    render(<App />);
+    const footerElement = screen.getByText(/© 1992 - 2020 Честный Агент © Все права защищены./);
+    expect(footerElement).toBeInTheDocument();
+    });
+})
